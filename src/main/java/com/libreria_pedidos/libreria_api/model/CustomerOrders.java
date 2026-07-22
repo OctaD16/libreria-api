496be97e725b2804725617d0cba6e9cbfd9ec3d0
OctaD <octad.dev@gmail.com>
2026-07-22 11:14:56 -0300

    Cambio en nombre de clases para evitar problemas en DB

---
+++ src/main/java/com/libreria_pedidos/libreria_api/model/CustomerOrders.java
@@ -0,0 +1,46 @@
+package com.libreria_pedidos.libreria_api.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+
+public class CustomerOrders {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private LocalDateTime date;
+
+    private String customerName;
+
+    private String address;
+
+    private String phone;
+
+    private String email;
+
+    private double total;
+
+    private Integer paymentMethod;
+
+    private Integer status;
+
+    private String observations;
+
+    private Integer deliveryType;
+
+    private LocalDateTime deliveryDate;
+
+
+
+
+
+
+
+}
---
+++ src/main/java/com/libreria_pedidos/libreria_api/model/OrdersDetails.java
@@ -0,0 +1,20 @@
+package com.libreria_pedidos.libreria_api.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class OrdersDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Integer quantity;
+
+    private double price;
+
+    private double subtotal;
+}
